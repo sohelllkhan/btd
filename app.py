ec999fc5a6b10f6ab8b3b58dca3db15e288ab17a
Sohel Khan <sohelkhan323526@gmail.com>
2026-05-14 20:24:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,15 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
 from PIL import Image
-import gdown
-import os
+import tflite_runtime.interpreter as tflite
 
-# -------------------------------
-# 1. DOWNLOAD MODEL FROM GOOGLE DRIVE (ONLY FIRST RUN)
-# -------------------------------
+# Load TFLite model
+interpreter = tflite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
 
-MODEL_PATH = "brain_tumor_model.keras"
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-if not os.path.exists(MODEL_PATH):
-    st.info("Downloading model... please wait")
-
-    url = "https://drive.google.com/file/d/1RLNXwoXA_0tMi36PP8EZmxsTrPAgLO6B/view?usp=drive_link"  # 🔴 replace this
-    gdown.download(url, MODEL_PATH, quiet=False, fuzzy=True)
-
-# -------------------------------
-# 2. LOAD MODEL
-# -------------------------------
-model = tf.keras.models.load_model(MODEL_PATH, compile=False)
-
-# -------------------------------
-# 3. UI
-# -------------------------------
 st.title("🧠 Brain Tumor Detection AI")
 
 upload = st.file_uploader("Upload MRI Image", type=["jpg", "png", "jpeg"])
@@ -35,13 +20,14 @@ if upload is not None:
     img = Image.open(upload).convert("RGB")
     st.image(img, caption="Uploaded Image", use_container_width=True)
 
-    # preprocess
     img = img.resize((128, 128))
-    img = np.array(img) / 255.0
+    img = np.array(img, dtype=np.float32) / 255.0
     img = np.expand_dims(img, axis=0)
 
-    # prediction
-    prediction = model.predict(img)
-    class_index = np.argmax(prediction)
+    interpreter.set_tensor(input_details[0]['index'], img)
+    interpreter.invoke()
+
+    output = interpreter.get_tensor(output_details[0]['index'])
+    class_index = np.argmax(output)
 
     st.success("Prediction: " + classes[class_index])
